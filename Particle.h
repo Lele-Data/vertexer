b52c820156f0514631c3cbc2aad31afdd009fe03
maciacco <mariociacco97@gmail.com>
2019-11-21 09:11:42 +0100

    add particle

---
+++ Particle.h
@@ -0,0 +1,25 @@
+// Particle.h
+// The class of an MC particle
+//
+// Authors: Mario Ciacco & Emanuele Data
+
+#include <TObject.h>
+
+class Particle : public TObject{
+ public:
+  Particle(double x=0.,double y=0.,double z=0.,double theta=0.,double phi=0.);
+  ~Particle();
+  void SetPoint();
+  void SetDirection();
+  double GetX() const;
+  double GetY() const;
+  double GetZ() const;
+  double GetTheta() const;
+  double GetPhi() const;
+ private:
+  double fX;
+  double fY;
+  double fZ;
+  double fTheta;
+  double fPhi;
+};
