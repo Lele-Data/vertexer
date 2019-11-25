5118c531721574c23a1bf95c3de2799a83f3d832
maciacco <mariociacco97@gmail.com>
2019-11-25 19:11:46 +0100

    re-add particle

---
+++ Particle.cxx
@@ -0,0 +1,33 @@
+// Particle.cxx
+// Implementation of Particle.h
+// 
+// Authors: Mario Ciacco & Emanuele Data
+
+#include <TObject.h>
+#include "Particle.h"
+
+ClassImp(Particle);
+
+Particle::Particle(double x,double y,double z,double theta,double phi):TObject(),
+fX(x),
+fY(y),
+fZ(z),
+fTheta(theta),
+fPhi(phi){
+  // constructor
+} 
+
+Particle::~Particle(){
+  // destructor
+}
+
+void Particle::SetPoint(double x,double y, double z){
+  fX = x;
+  fY = y;
+  fZ = z;
+}
+
+void Particle::SetDirection(double theta, double phi){
+  fTheta = theta;
+  fPhi = phi;
+}
