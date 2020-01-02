4a0f42a43450b6388df0e53c5023144728fab083
Lele-Data <scacco.arrocco@gmail.com>
2020-01-02 16:31:05 +0100

    add DrawHist.C

--- cfg/Constants.h
+++ cfg/Constants.h
@@ -57,7 +57,7 @@ const int nMaxInter=100;       // size of array for tracklets intersection with 
 // VERTEXER ALGORITHM PARAMETERS
 const double kDeltaPhi=0.005;  // phi slice width
 const double kZbinWidth=0.02;  // bin width
-const double kDeltaZ=0.20;     // horizontal cut
-const double kZwidth=0.10;     // centroid width
+const double kDeltaZ=0.10;     // horizontal cut
+const double kZwidth=0.20;     // centroid width
 
 #endif // CONSTANTS
