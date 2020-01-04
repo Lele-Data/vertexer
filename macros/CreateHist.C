8c41332e4c002ff2c910f0e67ed04cbc0631cfe7
maciacco <mariociacco97@gmail.com>
2020-01-04 12:18:15 +0100

    add fit ranges

--- macros/CreateHist.C
+++ macros/CreateHist.C
@@ -17,6 +17,8 @@
 #include "../src/Vertex.h"
 #include "../cfg/Constants.h"
 
+double fitRanges[][2]={{-700.,700.},{-600.,600.},{-600.,600.},{-500.,500.},{-500.,500.},{-400.,400.},{-300.,300.},{-300.,300.},{-300.,300.},{-300.,300.},{-300.,300.}};
+
 void CreateHist(std::string inFilename="recResult",std::string outFilename="HistResult_Eff_Reso"){
   std::string inFilename_ext=FILE_DIR+inFilename+".root";      // filename with *.root extension
   std::string outFilename_ext=FILE_DIR+outFilename+".root";    // filename with *.root extension
@@ -133,9 +135,7 @@ void CreateHist(std::string inFilename="recResult",std::string outFilename="Hist
     fitFun->SetParLimits(0,0.,1.e5);
     fitFun->SetParLimits(1,-50.,50.);
     fitFun->SetParLimits(2,0.,600.);
-    fitFun->SetLineColor(kBlue+3);
-    double hist_rms=projectionOnRes_mult->GetRMS();
-    projectionOnRes_mult->Fit(fitFun,"QLM+","",-1.5*hist_rms,1.5*hist_rms);
+    fitFun->SetLineColor(kBlue+3);projectionOnRes_mult->Fit(fitFun,"QLM+","",fitRanges[iMult-1][0],fitRanges[iMult-1][1]);
     projectionOnRes_mult->GetXaxis()->SetRangeUser(-4.*fitFun->GetParameter(2),4.*fitFun->GetParameter(2));
     projectionOnRes_mult->SetMarkerStyle(20);
     projectionOnRes_mult->SetMarkerColor(kRed);
