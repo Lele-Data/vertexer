187b2ac80d386a5f795385b5a96eb6600a326b19
Lele-Data <scacco.arrocco@gmail.com>
2020-01-05 16:00:53 +0100

    check deltaPhi correct

--- macros/SteerRec.C
+++ macros/SteerRec.C
@@ -15,62 +15,63 @@
 #include "../cfg/Constants.h"
 
 void SteerRec(std::string inFilename="simul",std::string outFilename="recResult",double deltaPhi=kDeltaPhi,double zBinWidth=kZbinWidth,double deltaZ=kDeltaZ,double zWidth=kZwidth,int meanNoiseSoft=kMeanNnoise){
-  if(deltaPhi<=0.05 && meanNoiseSoft<=100){ // check if deltaPhi or meanNoiseSoft is not too high to not have segmentation violation due to dimension of tracklets intersection array
-    std::string inFilename_ext=FILE_DIR+inFilename+".root";      // filename with *.root extension
-    std::string outFilename_ext=FILE_DIR+outFilename+".root";    // filename with *.root extension
-    
-    TStopwatch swatch;
-
-    // DECLARE MEMORY LOCATION
-    static VTX vert={0,0,0,0};
-    TClonesArray *hitsFirstLayer=new TClonesArray("Point2D",100);
-    TClonesArray *hitsSecondLayer=new TClonesArray("Point2D",100);
-
-    // OPEN FILES (IN/OUT)
-    TFile inFile(inFilename_ext.c_str());
-    TFile outFile(outFilename_ext.c_str(),"RECREATE");  // open a file (write mode)
-    if(!inFile.IsOpen()){
-      std::cout<<"No input file!"<<std::endl;
-      return;
-    }
-    
-    // INSTANTIATE NEW TREE TO SAVE VERTICES
-    TTree *vtxTree=new TTree(RecTreeName,"vertices");
-    static Vertex vtx(-999.f,-999.f,-999.f,-999.f,false);// memory location mapped to tree
-    vtxTree->Branch(RecVertBaranchName,&vtx);            // connect branch to the first memory location; specify types
-
-    // GET TREE FROM INPUT FILE
-    TTree *inTree=(TTree*)inFile.Get(SimulTreeName);
-    
-    // GET TREE BRANCHES FROM EXISTING TREE
-    TBranch *bVertMult=inTree->GetBranch(SimVertBranchName);
-    TBranch *bFirstLayer=inTree->GetBranch(SimHitFirstBranchName);
-    TBranch *bSecondLayer=inTree->GetBranch(SimHitSecondBranchName);
-
-    // SET ADDRESSES OF BRANCHES
-    bVertMult->SetAddress(&vert.X);
-    bFirstLayer->SetAddress(&hitsFirstLayer);
-    bSecondLayer->SetAddress(&hitsSecondLayer);
-
-    // INSTANTIATE LAYERS
-    Layer *layers[2];
-    layers[0]=new Layer(kFirstLayerRadius,kFirstLayerThick,kFirstLayerLength,kZresol,kRphiResol); // cm
-    layers[1]=new Layer(kSecondLayerRadius,kSecondLayerThick,kSecondLayerLength,kZresol,kRphiResol); // cm
-
-    // INSTANTIATE RECONSTRUCTION MANAGER AND RUN SIMULATION
-    RecManager *manager=RecManager::GetInstance(deltaPhi,zBinWidth,deltaZ,zWidth,meanNoiseSoft);
-    swatch.Start(); // start stopwatch
-    manager->RunReconstruction(inTree,vtxTree,vert,vtx,hitsFirstLayer,hitsSecondLayer,layers);
-    swatch.Stop();
-    swatch.Print("m");
-    manager=RecManager::Destroy();
-    
-    // WRITE AND CLOSE FILE
-    outFile.Write();
-    outFile.Close();
-    inFile.Close();
-  }else {
+  if(deltaPhi>0.05 || meanNoiseSoft>100){ // check if deltaPhi or meanNoiseSoft is not too high to not have segmentation violation due to dimension of tracklets intersection array
     std::cout<<"--- WARNING ---"<<std::endl;
     std::cout<<"Parameter DeltaPhi or MeanNnoise is too high"<<std::endl;
+    return;
   }
+  std::string inFilename_ext=FILE_DIR+inFilename+".root";      // filename with *.root extension
+  std::string outFilename_ext=FILE_DIR+outFilename+".root";    // filename with *.root extension
+    
+  TStopwatch swatch;
+
+  // DECLARE MEMORY LOCATION
+  static VTX vert={0,0,0,0};
+  TClonesArray *hitsFirstLayer=new TClonesArray("Point2D",100);
+  TClonesArray *hitsSecondLayer=new TClonesArray("Point2D",100);
+
+  // OPEN FILES (IN/OUT)
+  TFile inFile(inFilename_ext.c_str());
+  TFile outFile(outFilename_ext.c_str(),"RECREATE");  // open a file (write mode)
+  if(!inFile.IsOpen()){
+    std::cout<<"No input file!"<<std::endl;
+    return;
+  }
+    
+  // INSTANTIATE NEW TREE TO SAVE VERTICES
+  TTree *vtxTree=new TTree(RecTreeName,"vertices");
+  static Vertex vtx(-999.f,-999.f,-999.f,-999.f,false);// memory location mapped to tree
+  vtxTree->Branch(RecVertBaranchName,&vtx);            // connect branch to the first memory location; specify types
+
+  // GET TREE FROM INPUT FILE
+  TTree *inTree=(TTree*)inFile.Get(SimulTreeName);
+    
+  // GET TREE BRANCHES FROM EXISTING TREE
+  TBranch *bVertMult=inTree->GetBranch(SimVertBranchName);
+  TBranch *bFirstLayer=inTree->GetBranch(SimHitFirstBranchName);
+  TBranch *bSecondLayer=inTree->GetBranch(SimHitSecondBranchName);
+
+  // SET ADDRESSES OF BRANCHES
+  bVertMult->SetAddress(&vert.X);
+  bFirstLayer->SetAddress(&hitsFirstLayer);
+  bSecondLayer->SetAddress(&hitsSecondLayer);
+
+  // INSTANTIATE LAYERS
+  Layer *layers[2];
+  layers[0]=new Layer(kFirstLayerRadius,kFirstLayerThick,kFirstLayerLength,kZresol,kRphiResol); // cm
+  layers[1]=new Layer(kSecondLayerRadius,kSecondLayerThick,kSecondLayerLength,kZresol,kRphiResol); // cm
+
+  // INSTANTIATE RECONSTRUCTION MANAGER AND RUN SIMULATION
+  RecManager *manager=RecManager::GetInstance(deltaPhi,zBinWidth,deltaZ,zWidth,meanNoiseSoft);
+  swatch.Start(); // start stopwatch
+  manager->RunReconstruction(inTree,vtxTree,vert,vtx,hitsFirstLayer,hitsSecondLayer,layers);
+  swatch.Stop();
+  swatch.Print("m");
+  manager=RecManager::Destroy();
+    
+  // WRITE AND CLOSE FILE
+  outFile.Write();
+  outFile.Close();
+  inFile.Close();
+    
 }
